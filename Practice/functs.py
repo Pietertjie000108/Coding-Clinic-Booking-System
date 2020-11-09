cd7544726d2f00dc485b508dadadbbbd50529363
nmeintje <nmeintje@student.wethinkcode.co.za>
2020-11-09 15:00:47 +0200

    created main.py and added from functs to this file

--- Practice/functs.py
+++ Practice/functs.py
@@ -111,23 +111,4 @@ def volunteer_or_student():
     elif check.lower() == "create":
         return "volunteer"
 
-def main():
-    cred_gen()
-    service_gen()
-    show_event_id()
-    if volunteer_or_student() == "student":
-        pass
-    else :
-        summary = summary_input()
-        loc = location_input()
-        vol = user_email()
-        start_time_volunteer = time_slot_volunteer()
-        year,month,day,hour,minute,second = start_time_volunteer
-        date = datetime(year,month,day,hour,minute,second)
-        end_time_volunteer = date + timedelta(hours=1, minutes=30)
-        # print (date)
-        # print (end_time_volunteer)
-        # desc = desc_input()
-        create_event_voluteer_slots(summary, loc, vol, date, end_time_volunteer)
-if __name__ == '__main__':
-    main()
+
