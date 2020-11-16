2a312de3fb80af96b17d23090644021f50c7a52f
ndooge <ndooge@student.wethinkcode.co.za>
2020-11-16 14:23:46 +0200

    working on CLI

--- bash.sh
+++ bash.sh
@@ -1,9 +1,4 @@
-"""
-create venv
-pip install gapi
-create alias to run main.py using: booking_system <arg>
-"""
-pip3 install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+
 echo alias wtc-clinic="$PWD/wtc-clinic" >> ~/.bashrc
 echo alias wtc-clinic="$PWD/wtc-clinic" >> ~/.zshrc
 ​
