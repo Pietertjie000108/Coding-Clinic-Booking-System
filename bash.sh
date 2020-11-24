a20f005c17927a37b8a080ec2a4e19a5632a7240
Pietertjie000108 <p.pretorius000108@gmail.com>
2020-11-24 15:38:49 +0200

    updated bash.sh to install necessary modules to run program

--- bash.sh
+++ bash.sh
@@ -1,3 +1,5 @@
+pip3 install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+pip3 install rich
 
 alias wtc-clinic="$PWD/wtc-clinic" >> ~/.bashrc
 alias wtc-clinic="$PWD/wtc-clinic" >> ~/.zshr
