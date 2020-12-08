4aad67fe8b7a0312d8915a4cc1b58dda34af3f3a
Pietertjie000108 <73880035+Pietertjie000108@users.noreply.github.com>
2020-12-08 13:53:21 +0200

    Update bash.sh
    added pip install stdiomask

--- bash.sh
+++ bash.sh
@@ -1,6 +1,7 @@
 pip3 install whirlpool
 pip3 install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
 pip3 install rich
+pip3 install stdiomask
 
 
 alias wtc-clinic="$PWD/wtc-clinic" >> ~/.bashrc
