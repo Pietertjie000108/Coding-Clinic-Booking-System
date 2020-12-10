abaf795d233ae9b6ef396250e7ca91f3b64ce3ea
ndooge <ndooge@student.wethinkcode.co.za>
2020-12-10 11:24:38 +0200

    fixed file path for universal usage of 'wtc-clinic' cmd, display readme, and copy dir to fixed location on bash.sh

--- bash.sh
+++ bash.sh
@@ -1,14 +1,13 @@
+
 pip3 install whirlpool
 pip3 install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
 pip3 install rich
 pip3 install stdiomask
 
+cp $PWD/  ~/ -r
+alias wtc-clinic="~/Coding-Clinic-Booking-System/wtc-clinic" >> ~/.bashrc
 
-alias wtc-clinic="$PWD/wtc-clinic" >> ~/.bashrc
-alias wtc-clinic="$PWD/wtc-clinic" >> ~/.zshr
-
-
-chmod +x wtc-clinic
+chmod +x ~/Coding-Clinic-Booking-System/wtc-clinic
 
 
 echo $PWD
